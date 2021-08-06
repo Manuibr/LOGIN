464e4d48c9485c627ed627c76ac85be840d8308e
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2021-08-05 21:44:28 -0300

    Adicionada função de mostrar ou esconder senha na página de login

--- public/js/hidePassword.js
+++ public/js/hidePassword.js
@@ -1,5 +1,37 @@
-const senha = document.querySelector("#senha");
+const eyeIcon = document.querySelector("#eye");
+const senhaInput = document.querySelector("#senha");
+
+const faEye = document.querySelector("#eye #fa-eye");
+const faEyeSlash = document.querySelector("#eye #fa-eye-slash");
+
+faEye.addEventListener('click', function(event) {
+    event.preventDefault();
+    HidePassword();
+})
+
+faEyeSlash.addEventListener('click', function(event) {
+    event.preventDefault();
+    HidePassword();
+})
 
 function HidePassword() {
-    
+    if(senhaInput.type === 'password') {
+        senhaInput.type = 'text';
+        
+        faEyeSlash.classList.add('d-none');
+        faEye.classList.remove('d-none');
+        // faEye.classList.remove('d-none');
+        
+        // faEyeSlash.classList.add('d-none');
+        return;
+    }
+
+    senhaInput.type = 'password';
+    //faEye.classList.add('d-none');
+        
+        
+    faEye.classList.add('d-none');
+    faEyeSlash.classList.remove('d-none');
+    // faEyeSlash.classList.remove('d-none');
+    return;
 }
