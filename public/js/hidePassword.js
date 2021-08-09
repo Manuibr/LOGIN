f01ecd7113d9f7e29b0bfebd7b04aa65566cb3b3
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2021-08-09 20:36:17 -0300

    DIretório 'styles' refeito e arquivos css transformados em scss

--- public/js/hidePassword.js
+++ public/js/hidePassword.js
@@ -20,18 +20,13 @@ function HidePassword() {
         
         faEyeSlash.classList.add('d-none');
         faEye.classList.remove('d-none');
-        // faEye.classList.remove('d-none');
-        
-        // faEyeSlash.classList.add('d-none');
+
         return;
     }
 
     senhaInput.type = 'password';
-    //faEye.classList.add('d-none');
-        
-        
+
     faEye.classList.add('d-none');
     faEyeSlash.classList.remove('d-none');
-    // faEyeSlash.classList.remove('d-none');
     return;
 }
