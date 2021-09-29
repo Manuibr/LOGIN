8834d5986b5b74b1b4e6ade904d8e6f57beacc1a
nicolsam <nicolas.lorrayne@gmail.com>
2021-09-29 20:31:07 -0300

    teste

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 
-header("Location: public/pages/cadastro.html");
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/bootstrap/app.php';
+
+use \App\Entity\Receitas;
+
+$users = Receitas::getUsers();
+print_r($users);
+
